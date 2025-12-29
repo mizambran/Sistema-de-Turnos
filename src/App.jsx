26df8306f83aa15fb224be0ebb0447e87a0cc736
Miguel Angel Zambrano <angel245687@gmail.com>
2025-12-28 22:29:04 -0300

    maquetado de Menu y Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Menu from './components/shared/Menu'
+import Footer from './components/shared/Footer'
 
 
 function App() {
@@ -8,7 +10,9 @@ function App() {
 
   return (
     <>
-  
+    <Menu></Menu>
+
+    <Footer></Footer>
     </>
   )
 }
